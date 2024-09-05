698b23ade32cb06b8c5520d2937f9031fcc97e82
np03s190038 <thapa.sumit007@gmail.com>
2024-09-06 03:11:03 +0545

    image path fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-
+import reactIconSmall from '../assets/react-icon-small.png'
 
  function Navbar() {
     return (
         <nav>
-            <img src="{logo}" className="nav--icon" />
+            <img src={reactIconSmall} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
